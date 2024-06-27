21a3d1679b6fcfe4633f2eb4ee17c8b559b41561
Khan-Ramsha <khanramsha302020@gmail.com>
2024-06-28 00:46:53 +0530

    Dev Completed

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 
 # Route for home page
-@app.route("/")
-def index():
-    return render_template('index.html')
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
 
-@app.route("/predictdata",methods=['GET','POST'])
+@app.route("/",methods=['GET','POST'])
 def predict_data():
     if request.method == "GET":
         return render_template("home.html")
